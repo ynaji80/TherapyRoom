4259c551cd1054a6ff6d51ccd6c7f35c0ae4c0bd
ynaji80 <najiyounes80@gamil.com>
2022-12-04 13:11:48 +0100

    Initial configuration

---
+++ src/main/java/com/s5project/therapyroom/TherapyRoomApplication.java
@@ -0,0 +1,13 @@
+package com.s5project.therapyroom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TherapyRoomApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TherapyRoomApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/s5project/therapyroom/TherapyRoomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.s5project.therapyroom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TherapyRoomApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
